129726190fcfab635c76c06b6e6865893595480c
dinoscode <dinostatoo@gmail.com>
2025-06-04 10:16:04 -0300

    Teste estudos

---
+++ seq.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+
+for i in $(seq 5);
+do
+        clear
+        echo -ne "\n\n\nContando $iº: $i\n"
+        sleep 1
+done
+
---
+++ umrgx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+
+filename="filex.txt"
+
+if [[ "$filename" =~ \.txt$ ]]; then
+        echo "Filename has a .txt extension"
+else
+        echo "Filename does not have a .txt extension"
+fi
+
